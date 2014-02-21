b170b9ced06c3aba66ca40b3ee2d2f21383cbb83
schaubce <schaubce@uwec.edu>
2014-02-21 13:50:59 -0600

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,43 +3,94 @@ require 'spec_helper'
 describe "UserPages" do
   subject { page }
   
-  describe "profile page" do
-     let(:user)  {FactoryGirl.create(:user) }
-     before { visit user_path(user) }
-
-     it { should have_selector('h1',    :text=> user.name) }
-     it { should have_selector('title', :text=> user.name) }
-  end
-
-  describe "signup page" do 
-    before { visit signup_path }
+  describe "User pages" do
     
-    it { should have_selector('h1', :text=> 'Sign up') }
-    it { should have_selector('title', :text=> full_title('Sign up')) } 
-  end
-  
-  describe "signup" do 
+    subject { page }
 
-    before { visit signup_path }
-    
-    let(:submit) {"Create my account"}
+    describe "index" do
+        
+      let (:user) {FactoryGirl.create(:user) }
 
-    describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
+      before(:each) do
+          sign_in user
+          visit users_path
+      end
+
+      it { should have_selector('title', :text=> 'All users') }
+      it { should have_selector('h1', :text=> 'All users') }
+
+      describe "pagination" do
+          
+        before(:all) { 30.times {FactoryGirl.create(:user) } }
+        after(:all) {User.delete_all }
+
+        it {should have_selector('div', :class=>'paginiation') }
+
+        it "should list each user" do
+          User.paginate(:page=> 1).each do |user|
+            page.should have_selector('li', :text=> user.name)
+          end
+        end
+      end
+
+      describe "delete links" do
+
+        it { should_not have_link('delete') }
+
+        describe "as an admin user" do
+          let(:admin) { FactoryGirl.create(:admin) }
+          before do
+            sign_in admin
+            visit users_path
+          end
+
+         it { should have_link('delete', :href=> user_path(User.first)) }
+         it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+         end
+         it { should_not have_link('delete', :href=> user_path(admin)) }
+        end
       end
     end
 
-    describe "with valid information" do
-      before do
-        fill_in "Name",         :with=> "Example User"
-        fill_in "Email",        :with=> "user@example.com"
-        fill_in "Password",     :with=> "foobar"
-        fill_in "Confirmation", :with=> "foobar"
+    describe "profile page" do
+       let(:user)  {FactoryGirl.create(:user) }
+       before { visit user_path(user) }
+
+       it { should have_selector('h1',    :text=> user.name) }
+       it { should have_selector('title', :text=> user.name) }
+    end
+
+    describe "signup page" do 
+      before { visit signup_path }
+    
+      it { should have_selector('h1', :text=> 'Sign up') }
+      it { should have_selector('title', :text=> full_title('Sign up')) } 
+    end
+  
+    describe "signup" do 
+
+      before { visit signup_path }
+    
+      let(:submit) {"Create my account"}
+
+      describe "with invalid information" do
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
       end
 
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+      describe "with valid information" do
+        before do
+          fill_in "Name",         :with=> "Example User"
+          fill_in "Email",        :with=> "user@example.com"
+          fill_in "Password",     :with=> "foobar"
+          fill_in "Confirmation", :with=> "foobar"
+        end
+
+        it "should create a user" do
+          expect { click_button submit }.to change(User, :count).by(1)
+        end
       end
     end
   end
