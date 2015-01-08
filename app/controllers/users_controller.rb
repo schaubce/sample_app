81f321632c9303a9e1d6e43ef77cdb7124f4c6ec
schaubce <schaubce@uwec.edu>
2015-01-08 09:52:36 -0600

    Update users_controller.rb
    Fixed formatting - it was bugging me

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def edit
   end
 
-	def show
+  def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(:page=> params[:page])
   end
@@ -35,26 +35,26 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-	def edit
-		@user = User.find(params[:id])
-	end
+  def edit
+     @user = User.find(params[:id])
+  end
 
-	def update
-		@user = User.find(params[:id])
-		if @user.update_attributes(params[:user])
-			flash[:success] = "Profile updated"
-			sign_in @user
-			redirect_to @user
-		else
-			render 'edit'
-		end
-	end
-
-	private
-		def correct_user
-		  @user = User.find(params[:id])
-		  redirect_to(root_url) unless current_user?(@user)
-    end
+  def update
+     @user = User.find(params[:id])
+     if @user.update_attributes(params[:user])
+        flash[:success] = "Profile updated"
+	sign_in @user
+	redirect_to @user
+     else
+	render 'edit'
+     end
+   end
+	
+   private
+   def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+   end
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
